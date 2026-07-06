4ba9e6aaab87b3c486efcc3c2caf18002ee0fe68
ewy-hash <ewyers@students.pitzer.edu>
2026-07-06 12:14:24 -0700

    Update api-update-pubs.R

--- data-and-scripts/api-update-pubs.R
+++ data-and-scripts/api-update-pubs.R
@@ -1,5 +1,4 @@
 #update pubs using web of science API
-
 library(tidyverse)
 library(httr2)
 library(jsonlite)
@@ -9,13 +8,14 @@ library(countrycode)
 library(openxlsx)
 
 
-
-
 token <- Sys.getenv("API_TOKEN")
 
 baseURL <- "https://api.clarivate.com/apis/wos-starter/v2"
 
 token <- Sys.getenv("API_TOKEN")
+
+author_id <- 29169926
+
 
 #still modifying this
 allAPI <- request(baseURL)|> 
@@ -25,3 +25,11 @@ allAPI <- request(baseURL)|>
   #req_dry_run()
   req_perform()|> 
   resp_body_json(simplifyVector = TRUE)
+
+
+#this is to change all of the list columns out to be their own columns
+approachesWIDE <- approaches |> 
+  unnest_wider(where(is.list), names_sep = "_") |> 
+  unnest_wider(where(is.list), names_sep = "_") |> 
+  unnest_wider(where(is.list), names_sep = "_") |> 
+  unnest_wider(where(is.list), names_sep = "_")
